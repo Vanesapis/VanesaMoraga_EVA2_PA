7fcdf38c517ed9bcf46c21d0655df860ac0efa2f
vanes <vanes@DESKTOP-5MJAMG9>
2022-06-13 02:58:41 -0400

    Add project files.

---
+++ VanesaMoraga_EVA2/Controllers/EstadisticaController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using VanesaMoraga_EVA2.Models;
+
+namespace VanesaMoraga_EVA2.Views.Estadistica
+{
+    public class EstadisticaController : Controller
+    {
+        private FabricaDB db = new FabricaDB();
+
+        // GET: Estadistica
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
